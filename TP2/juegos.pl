3ea16a66018eb57121fe73e8df77572a92b705d2
Paula Borrero <paulajessicab@gmail.com>
2017-04-26 16:11:15 -0300

    ivan

--- TP2/juegos.pl
+++ TP2/juegos.pl
@@ -3,6 +3,7 @@
 digital games identification game.
 start with ?- go.
 */
+
 go :- hypothesize(Game),
 write('I guess that the game is: '),
 write(Game),
@@ -22,34 +23,54 @@ hypothesize(dark_souls)
 :- dark_souls, !.
 hypothesize(minecraft)
 :- minecraft, !.
-hypothesize(agregar).
+hypothesize(agregar) :- agregar,!.
 
 /*hypothesize(penguin)
 :- penguin, !.
 hypothesize(albatross) :- albatross, !.
 */
 
+append([fps(first_person,shooter),online(multiplayer),moba(battle_arena,has_minions,online),action,strategy,role,science_fiction,third_person,open_world,
+terror,survival,fantasy ],[puzzle],Attributes).
 
+attr([fps(first_person,shooter),online(multiplayer),moba(battle_arena,has_minions,online),action,strategy,role,science_fiction,third_person,open_world,
+terror,survival,fantasy,puzzle]).
 /* no diagnosis */
 /* games identification rules */
+
 call_of_duty :- fps,
 verify(action),online.
 
-overwatch :- fps,
-moba.
+overwatch :- fps,online.
 
 league_of_legends :- moba,
-verify(stratefy). 
+verify(strategy). 
 
-mass_effect :- verify(role),verify(science_fiction),verify(third_person),verify(action),verify(shooter).
+mass_effect :- verify(role),verify(science_fiction),verify(third_person),verify(open_world),verify(action),verify(shooter).
 
 the_witcher_3 :- 
-verify(role),verify(fantasy),verify(action),verify(third_person).
+verify(role),verify(fantasy),verify(action),verify(third_person),verify(open_world).
 
 dark_souls :-
-verify(survival),verify(terror),verify(third_person).
+verify(survival),verify(fantasy),verify(terror),verify(third_person).
 
-minecraft :- verify(survival).
+minecraft :- verify(survival),verify(open_world).
+
+devil_may_cry :- 
+third_person, verify(action), verify(fantasy).
+
+half_life :-
+fps, verify(action), verify(science_fiction).
+
+dead_space :-
+verify(third_person),verify(shooter), verify(terror), verify(science_fiction), verify(survival).
+
+gta :-
+verify(third_person),verify(shooter), verify(open_world),verify(action).
+
+portal :-
+verify(first_person),verify(puzzle),verify(science_fiction).
+assert(Game :- verify(Attr)),assert(Game :- verify(Attr1))
 
 fps :- verify(first_person),verify(shooter).
 online :- verify(online),verify(multiplayer).
@@ -84,4 +105,15 @@ agregar_ok :-
 write('The name of the game is: '),nl,
 read(Game),
 write('What attribute the game has?'),nl,
+write('[fps(first_person,shooter),online(multiplayer),moba(battle_arena,has_minions,online),action,strategy,'),
+write('role,science_fiction,third_person,open_world,terror,survival,fantasy,puzzle]'),nl.
+
+write_list([]).
+write_list([H|T]) :-
+	write(H),nl,
+	write_list(T).
+
+read_list(List) :-
+write('Attribute: '),
 read(Att),
+(Att == finish ; Att == f) -> List ; append(List,[Att],NewList),read_list(NewList).
